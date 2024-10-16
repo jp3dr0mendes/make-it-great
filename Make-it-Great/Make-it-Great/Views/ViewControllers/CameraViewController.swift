2662894e8e52a08630763deb05a71a922ed9b4c6
joao pedro <joao.pedro73210@gmail.com>
2024-10-16 11:02:56 -0300

    yane se der errado eu me mato na sua frente

--- Make-it-Great/Make-it-Great/Views/ViewControllers/CameraViewController.swift
+++ Make-it-Great/Make-it-Great/Views/ViewControllers/CameraViewController.swift
@@ -7,10 +7,53 @@
 
 import SwiftUI
 import AVFoundation
+import CoreML
+import Vision
+import UIKit
 
-class CameraViewController: UIViewController {
-    var captureSession: AVCaptureSession?
-    var previewLayer: AVCaptureVideoPreviewLayer?
+class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+//    var image: UIImage?
+    private var captureSession: AVCaptureSession?
+    private var previewLayer: AVCaptureVideoPreviewLayer?
+    private var previewFrame: CVPixelBuffer?
+    var imageBuffer: CVImageBuffer?
+    
+    @Binding var classification: String
+    
+    init(classification: Binding<String>) {
+        self._classification = classification
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func imageClassifier(frame: CVPixelBuffer) {
+        //CVPixelBuffer
+        guard let model = try? VNCoreMLModel(for: MyImageClassifier_Test3().model) else {
+                    print("Erro ao carregar o modelo")
+                    return
+                }
+        
+        var request = VNCoreMLRequest(model: model){ request, error in
+            print(request)
+            guard let results = request.results as? [VNClassificationObservation] else {
+                return
+            }
+             guard let classification = results.first?.identifier else {
+                return
+            }
+            
+            self.classification = classification
+        }
+        
+        
+        
+        let handler = VNImageRequestHandler(cvPixelBuffer: frame)
+        
+        try! handler.perform([request])
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,33 +74,67 @@ class CameraViewController: UIViewController {
         
         captureSession.commitConfiguration()
         
+        //desenvolver a logica de classificar a imagem com base no create ml
+        
         //Configura o preview para mostrar a camera na tela:
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer?.videoGravity = .resizeAspectFill
         previewLayer?.frame = view.bounds
+        
+        
+        let videoCaptureOutput = AVCaptureVideoDataOutput()
+        videoCaptureOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        
+        captureSession.addOutput(videoCaptureOutput)
+        
+        videoCaptureOutput.videoSettings = [
+            (kCVPixelBufferPixelFormatTypeKey as String): kCVPixelFormatType_32BGRA
+
+        ]
+        
+
+//        self.imageBuffer = imageBuffer
+
+        
         if let previewLayer {
             view.layer.addSublayer(previewLayer)
         }
         
         //Começa a capturar:
-        captureSession.startRunning()
+        DispatchQueue.global().async {
+            captureSession.startRunning()
+            print("aqui")
+        }
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let frame = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        imageClassifier(frame: frame)
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         previewLayer?.frame = view.bounds
+        
+        
+//        imageClassifier(image: previewLayer?.frame)
     }
 }
 
 struct CameraView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> CameraViewController {
-        return CameraViewController()
+    
+    @Binding var classification: String
+    
+    func makeUIViewController(context: Context) -> CameraViewController{
+        print("sldkfsd")
+        return CameraViewController(classification: $classification)
     }
     
     func updateUIViewController(_ uiViewController: CameraViewController, context: Context) {
         //Aqui é para atualizar a view caso precise
     }
-    
 }
 
 
