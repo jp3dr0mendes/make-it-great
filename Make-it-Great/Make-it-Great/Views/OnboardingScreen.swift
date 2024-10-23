49f2de2f2a3dffcc78ef4bc8ec0f43f210c0d57f
Daniel Barros <daniel.victor.damasceno05@aluno.ifce.edu.br>
2024-10-23 08:22:20 -0300

    Onboarding Screen cria arquivo

---
+++ Make-it-Great/Make-it-Great/Views/OnboardingScreen.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingScreen.swift
+//  Make-it-Great
+//
+//  Created by Daniel Barros on 23/10/24.
+//
+
+import SwiftUI
+
+struct OnboardingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingScreen()
+}
