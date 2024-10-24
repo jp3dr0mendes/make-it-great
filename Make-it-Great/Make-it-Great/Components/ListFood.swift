cfdf3f16aafc4ee48a28ea0b589c913059227ddc
Yane <yanesantos2001@hotmail.com>
2024-10-24 11:26:24 -0300

    resolução conflito

--- Make-it-Great/Make-it-Great/Components/ListFood.swift
+++ Make-it-Great/Make-it-Great/Components/ListFood.swift
@@ -10,11 +10,11 @@ import SwiftData
 
 struct ListFood: View {
     @Environment(\.modelContext) var context
-//    var comida: Food
+    //    var comida: Food
     //Lista de alimentos:
-//    @State var comidas: [Food]
+    //    @State var comidas: [Food]
     //Armazena alimetos selecionados:
-//    @State var selectedItems: Set<Food> = []
+    //    @State var selectedItems: Set<Food> = []
     // Lista de alimentos (agora um binding)
     @Binding var comidas: [Food]
     // Armazena alimentos selecionados (agora um binding)
@@ -46,71 +46,75 @@ struct ListFood: View {
                         }
                     }/*.padding(.leading, 10)*/ //Afasta o checkbox da imagem, acho que esse padding é desnecessario
                     Button( action: { isPresentedSheet = true }) {
-                    //Spacer()
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 30)
-                            .stroke(selectedCategory == .refrigerator ? Color(.brownFruits) : Color(.greenVegetables), lineWidth: 1)
-                            .frame(width: 45, height: 45)
-                        if comida.emoji == "" {
-                            Text("\(comida.storage == "Geladeira" ? "🍎" : "🥕")")
-                                .font(.system(size: 30))
-                        } else {
-                            Text("\(comida.emoji ?? "")")
-                                .font(.system(size: 30))
+                        //Spacer()
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 30)
+                                .stroke(selectedCategory == .refrigerator ? Color(.brownFruits) : Color(.greenVegetables), lineWidth: 1)
+                                .frame(width: 45, height: 45)
+                            if comida.emoji == "" {
+                                Text("\(comida.storage == "Geladeira" ? "🍎" : "🥕")")
+                                    .font(.system(size: 30))
+                            } else {
+                                Text("\(comida.emoji ?? "")")
+                                    .font(.system(size: 30))
+                            }
                         }
-                    }
-                    .padding(2)
-                    
-
-//                    Image("imageTest") //Preciso fazer um model para colocar a imagem certa aqui
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fit)
-//                        .frame(width: 40, height: 40)
-//                        .clipShape(Circle())
-//                    //.foregroundColor(.yellow)
-//                        .overlay(Circle().stroke(selectedCategory == .refrigerator ? Color(.brownFruits) : Color(.greenVegetables), lineWidth: 1))
-//                        .shadow(radius: 10)
-                    //Nome da Comida e Quantidade:
-                    VStack (alignment: .leading) {
-                        Text(comida.nome)
-                            .font(.callout)
-                            .multilineTextAlignment(.leading)
-                            .lineLimit(1)
-                            .truncationMode(.tail)
-                        if comida.units != nil {
-                            Text("\(comida.units ?? 0)x")
-                                .font(.subheadline)
-                                .foregroundColor(.gray)
-                        } else {
-                            Text("\(String(format: "%.2f", comida.weight ?? 0.0))kg")
-                                .font(.subheadline)
-                                .foregroundColor(.gray)
-                        }
-                        //Preciso colocar a lógica de deletar
-//                        Button("Apagar"){
-//                            context.delete(comida)
-//                        }
+                        .padding(2)
                         
-                    }//.padding(.leading, 10)
-                    
-                    
-                    Spacer(minLength: 10) //Esse Spacer afasta o nome da comida e os dias alguns pixels, não sei se é tão necessário.
-                    Text(calculoDias(dataFim: comida.consumirAte ?? Date()))
-                        .font(.caption)
-                        .multilineTextAlignment(.trailing)
-                        .foregroundColor(.gray)
-                       }
-                   EditItemSheet(isPresented: $isPresentedSheet, item: comida, peso: comida.weight ?? 0.0, unidades: comida.units ?? 0)
+                        
+                        //                    Image("imageTest") //Preciso fazer um model para colocar a imagem certa aqui
+                        //                        .resizable()
+                        //                        .aspectRatio(contentMode: .fit)
+                        //                        .frame(width: 40, height: 40)
+                        //                        .clipShape(Circle())
+                        //                    //.foregroundColor(.yellow)
+                        //                        .overlay(Circle().stroke(selectedCategory == .refrigerator ? Color(.brownFruits) : Color(.greenVegetables), lineWidth: 1))
+                        //                        .shadow(radius: 10)
+                        //Nome da Comida e Quantidade:
+                        VStack (alignment: .leading) {
+                            Text(comida.nome)
+                                .font(.callout)
+                                .multilineTextAlignment(.leading)
+                                .lineLimit(1)
+                                .truncationMode(.tail)
+                            if comida.units != nil {
+                                Text("\(comida.units ?? 0)x")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            } else {
+                                Text("\(String(format: "%.2f", comida.weight ?? 0.0))kg")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                            //Preciso colocar a lógica de deletar
+                            //                        Button("Apagar"){
+                            //                            context.delete(comida)
+                            //                        }
+                            
+                        }//.padding(.leading, 10)
+                        
+                        
+                        Spacer(minLength: 10) //Esse Spacer afasta o nome da comida e os dias alguns pixels, não sei se é tão necessário.
+                        Text(calculoDias(dataFim: comida.consumirAte ?? Date()))
+                            .font(.caption)
+                            .multilineTextAlignment(.trailing)
+                            .foregroundColor(.gray)
+                        
                     }
-                       // .padding(.trailing, 10) //Esse pading afasta o 30 dias... da extremidade direita
-                    //Spacer()
+                    .sheet(isPresented: $isPresentedSheet) {
+                        EditItemSheet(isPresented: $isPresentedSheet, item: comida, peso: comida.weight ?? 0.0, unidades: comida.units ?? 0)
+                    }
+                    
+//                    EditItemSheet(isPresented: $isPresentedSheet, storage: $selectedCategory, item: comida, peso: comida.weight ?? 0.0, unidades: comida.units ?? 0)
                 }
-                //.padding(.leading, 10)
-                
-                Divider() //Linha horizontal que divide os elementos
+                // .padding(.trailing, 10) //Esse pading afasta o 30 dias... da extremidade direita
+                //Spacer()
             }
+            //.padding(.leading, 10)
             
+            Divider() //Linha horizontal que divide os elementos
         }
+        
     }
     
     private func calculoDias(dataFim: Date) -> String {
@@ -140,6 +144,7 @@ struct ListFood: View {
             selectedItems.insert(comida)
         }
     }
+}
     
     //Funcao que é chamada quando aperta o botao para deletar os itens:
 //    func deleteSelectedItems() {
@@ -152,7 +157,6 @@ struct ListFood: View {
 //        }
 //        selectedItems.removeAll()
 //    }
-}
 
 //#Preview {
 //    ListFood(comida: Food(nome: "Item food visualization Named sdfads dsf asd fads dafs adf", storage: .refrigerator, type: .Bebida, consumirAte: Date(timeInterval: 7*60*60*24, since: Date.now), units: 2, weight: nil))
